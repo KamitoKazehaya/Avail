7ce5422aa7ef00cb277fa15ad27665585a4471c8
Avail <kamitokazahey@gmail.com>
2016-09-03 22:02:42 +0300

    Update setting.php

--- avail/config/setting.php
+++ avail/config/setting.php
@@ -3,3 +3,8 @@
 define('APP',  'avail');//default app name (directory /app/avail/) else make dir in app (/app/forum/ APP = forum)
 define('URL', 'http://127.0.0.1/');
 define('SITE', 'Avail');
+
+//datebase conn setting
+define('DB_H', 'localhost');//host
+define('DB_U', 'root');//user
+define('DB_P', '12341234');//password
